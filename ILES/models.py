4d404cdf1239f3ec7a73423aed0f984a79a36b29
naluyimalaureen-source <naluyimalaureen@gmail.com>
2026-03-13 11:20:06 +0300

    these are the Academic Evaluation models

--- ILES/models.py
+++ ILES/models.py
@@ -1,6 +1,26 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from .models import InternshipPlacement,CustomUser
 
 # Create your models here.
-class CustomUser(AbstractUser):
-    pass
+class AcademicEvaluation(models.Model):
+    placement=models.ForeignKey(InternshipPlacement, on_delete=models.CASCADE)
+    evaluator=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+    supervisor_score=models.DecimalField(max_digits=5,decimal_places=2,default=0)
+    technical_skills=models.IntegerField(default=0)
+    communication_skills=models.IntegerField(default=0)
+    problem_solving=models.IntegerField(default=0)
+    attendance=models.IntegerField(default=0)
+    total_score=models.DecimalField(max_digits=5, decimal_places=2,blank=True,null=True)
+    overall_comment=models.TextField()
+    date_and_time_evaluated=models.DateTimeField(auto_now_add=True)
+    
+    def save(self,*args,**kwargs):
+        self.total_score=(self.technical_skills+self.communication_skills
+                          +self.problem_solving+ self.attendance+self.supervisor_score)
+        super().save(*args,**kwargs)
+    
+    def __str__(self):
+        return f"Evaluation for{self.placement}-Total:{self.total_score}"
+    
+    
