f57d831f2454a12f69807811f9a7e135d9ba0d85
priscillanabachwa <priscillanabachwa4@gmail.com>
2026-03-17 18:12:00 +0300

    Added the CustomUser model

--- backend/ILES/models.py
+++ backend/ILES/models.py
@@ -6,7 +6,26 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    pass
+    ROLE_CHOICES = [
+        ('student', 'Student Intern'),
+        ('workplace_supervisor', 'Workplace Supervisor'),
+        ('academic_supervisor', 'Academic Supervisor'),
+        ('admin', 'Internship Administrator'),
+    ]
+    role = models.CharField(
+        max_length=30, 
+        choices=ROLE_CHOICES, 
+        default='student'
+    )
+    phone_number = models.CharField(max_length=15, blank=True)
+    profile_picture = models.ImageField(
+        upload_to = 'profile_pictures/',
+        blank = True,
+        null = True
+    )
+
+    def __str__(self):
+        return f'{self.username} ({self.role})'
 
 class InternshipPlacement(models.Model):
     STATUS_CHOICE = [
