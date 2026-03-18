be9cbefe8fbcc39395cc3f0f745d9a4952c77bfe
priscillanabachwa <priscillanabachwa4@gmail.com>
2026-03-18 13:26:30 +0300

    Registered the models in the admin panel

--- backend/ILES/admin.py
+++ backend/ILES/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CustomUser, InternshipPlacement, WeeklyLogbook, AcademicEvaluation, WeightedScoreComputation
+
+admin.site.register(CustomUser)
+admin.site.register(InternshipPlacement)
+admin.site.register(WeeklyLogbook)
+admin.site.register(AcademicEvaluation)
+admin.site.register(WeightedScoreComputation)
