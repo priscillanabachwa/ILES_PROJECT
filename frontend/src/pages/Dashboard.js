3f1e287b93f58751e0ec3fd0868304cb7d4f7885
naluyimalaureen-source <naluyimalaureen@gmail.com>
2026-03-25 10:06:14 +0300

    updated dashboard

---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,10 @@
+function Dashboard(){
+    return(
+        <div>
+            <h1>Welcome to ILES</h1>
+            <P> You are loggged in as a student.</P>
+        </div>
+
+    );
+}
+export default Dashboard;
