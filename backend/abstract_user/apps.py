e6ea235ba2eafbf24a36a35f3b46bb16c3e98a88
lancer-freeman <kizitojumalancer@gmail.com>
2026-03-26 18:00:21 +0300

    abstract user model

---
+++ backend/abstract_user/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AbstractUserConfig(AppConfig):
+    name = 'abstract_user'
---
+++ backend/abstract_user/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('student', 'Student Intern'),
+        ('workplace_supervisor', 'Workplace Supervisor'),
+        ('academic_supervisor', 'Academic Supervisor'),
+        ('admin', 'Internship Administrator'),
+    ]
+    role = models.CharField(
+        max_length=30, 
+        choices=ROLE_CHOICES, 
+        default='student'
+    )
+    phone_number = models.CharField(max_length=15, blank=True)
+    profile_picture = models.ImageField(
+        upload_to = 'profile_pictures/',
+        blank = True,
+        null = True
+    )
+
+    def __str__(self):
+        return f'{self.username} ({self.role})'
+
+
+# Create your models here.
