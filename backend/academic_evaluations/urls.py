20f0ed1d6417a0881e00dd98a8b9c5cc60027a70
priscillanabachwa <priscillanabachwa4@gmail.com>
2026-04-08 18:11:23 +0300

    created urls.py files for the apps

---
+++ backend/academic_evaluations/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path, include
+
+urlpatterns = [
+    # Your evaluation routes will go here
+]
---
+++ backend/placements/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path, include
+
+urlpatterns = [
+    # Leave this empty for now or add your placement routes later
+]
