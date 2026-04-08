18efbca480bbfe911373400d2f33f71c5cc24905
lancer-freeman <kizitojumalancer@gmail.com>
2026-04-08 20:06:27 +0300

    aadded weekly logs and serializers

--- backend/user_accounts/serializers .py
+++ backend/user_accounts/serializers .py
@@ -1,12 +1,39 @@
+
 from rest_framework import serializers
+
 from .models import CustomUser
+
+
 class CustomUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, min_length=8)
+
     class Meta:
         model = CustomUser
-        fields = '__all__'
-        extra_kwargs = {
-            'password':{'write_only':True},
-            'phone_number':{'write_only': True},
-            'email':{'write_only':True}
-        }
-         
+        fields = [
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "username",
+            "role",
+            "phone_number",
+            "profile_picture",
+            "password",
+        ]
+        read_only_fields = ["id"]
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = CustomUser(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
