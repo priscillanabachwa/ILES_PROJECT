5b03d488b5ed3ead4d2f44701ba3212d0ea7b494
lancer-freeman <kizitojumalancer@gmail.com>
2026-04-08 20:06:07 +0300

    aadded weekly logs and serializers

--- backend/weighted_score_computation/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin
-from .models import WeightedScoreComputation
-admin.site.register(WeightedScoreComputation)
--- backend/weighted_score_computation/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WeightedScoreComputationConfig(AppConfig):
-    name = 'weighted_score_computation'
