60c7439f3d337d04b88318ced177606d6fbb2810
priscillanabachwa <priscillanabachwa4@gmail.com>
2026-04-09 21:52:37 +0300

    configured PostgreSQL in django

--- backend/ILES/settings.py
+++ backend/ILES/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'iles.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'iles_db',
+        'USER': 'iles_user',
+        'PASSWORD': 'iles_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
