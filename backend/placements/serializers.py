4220b3d2a9a3235c6747e700a623b9a29916ae65
naluyimalaureen-source <naluyimalaureen@gmail.com>
2026-04-11 21:03:06 +0300

    added validation method  to the placement model

--- backend/placements/serializers.py
+++ backend/placements/serializers.py
@@ -7,3 +7,19 @@ class PlacementSerializer(serializers.ModelSerializer):
     class Meta:
         model = InternshipPlacement
         fields = ['id', 'student', 'student_username', 'company_name', 'start_date', 'end_date', 'status']
+        
+    def validate(self,data):
+        if data ['end_date']<=data['start_date']:
+            raise serializers.ValidationError(
+                'End date must be after Start date'
+            )
+        student =data['student']
+        overlapping = InternshipPlacement.objects.filter(
+            student=student
+            status='active'
+        )
+        if overlapping.exists():
+            raise serializers.ValidationError(
+                'This student already has an active placement'
+            )
+        return data
