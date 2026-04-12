6474c6542728a652b9cce119cc548d0acd5c22e9
priscillanabachwa <priscillanabachwa4@gmail.com>
2026-04-12 08:36:33 +0300

    added a deadline validation in weeklylog serializer

--- backend/weekly_logs/serializer.py
+++ backend/weekly_logs/serializer.py
@@ -6,7 +6,21 @@ from .models import WeeklyLogbook
 class WeeklyLogbookSerializer(serializers.ModelSerializer):
     
     class Meta:
-        model = WeeklyLogbook
+        model = WeeklyLog
         fields = '__all__'
+
         read_only_fields = ['created_at','submitted_at','updated_at']
-  
+
+    def validate(self, date):
+        if 'status' in data and data ['status'] == 'submitted':
+            if data.get('deadline') and date.today() > data['deadline']:
+                raise serializers.ValidationError(
+                    'Cannot submit a log after the deadline'
+                )
+        
+        if self.instance and self.instance.status == 'approved':
+            raise serializers.ValidationError(
+                'Cannot edit an approved log'
+            )
+    
+        return data
