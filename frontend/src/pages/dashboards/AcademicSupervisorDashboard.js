6b594487f6992d89d9fa682fdca7e483d21bd8c7
atwiinesheila02-web <atwiinesheila02@gmail.com>
2026-04-12 10:10:14 +0100

    Added content to dashdoardservice

--- frontend/src/pages/dashboards/AcademicSupervisorDashboard.js
+++ frontend/src/pages/dashboards/AcademicSupervisorDashboard.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useAuth } from '../context/AuthContext'
 import { Link, useNavigate } from 'react-router-dom'
-import { dashboardService } from '../services'
+import { AcademicSupervisorDashboardservice } from '../services'
 
 
 const formatDate = (iso) =>
