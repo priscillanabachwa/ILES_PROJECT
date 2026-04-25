2452825d86d95b583133ea2525fb3182fc133954
naluyimalaureen-source <naluyimalaureen@gmail.com>
2026-04-25 20:37:02 +0300

    added content to the login.js form.

--- backend/ILES/settings.py
+++ backend/ILES/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'ILES.urls'
+ROOT_URLCONF = 'iles.urls'
 
 TEMPLATES = [
     {
@@ -77,7 +77,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'ILES.wsgi.application'
+WSGI_APPLICATION = 'iles.wsgi.application'
 
 
 # Database
