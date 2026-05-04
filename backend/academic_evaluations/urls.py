c1d7d8a7fab16c804ae141a312cfd9e327c48453
priscillanabachwa <priscillanabachwa4@gmail.com>
2026-05-04 12:59:59 +0300

    updated the academic evaluations urls.py

--- backend/academic_evaluations/urls.py
+++ backend/academic_evaluations/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AcademicEvaluationViewSet 
+from .views import AcademicEvaluationViewSet, EvaluationCriteriaViewSet, EvaluationScoreViewSets
 
 router = DefaultRouter()
-router.register(r'scores', AcademicEvaluationViewSet, basename='evaluation')
+router.register(r'evaluations', AcademicEvaluationViewSet, basename='evaluation')
+router.register(r'criteria', EvaluationCriteriaViewSet, basename='criteria')
+router.register(r'scores', EvaluationScoreViewSets, basename='scores')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
 ]
