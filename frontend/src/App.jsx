8106191b003c8dc87234c7a5934782181bedf421
priscillanabachwa <priscillanabachwa4@gmail.com>
2026-05-04 17:48:54 +0300

    added a safety check in the authcontext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.jsx'
 import AppLayout from './components/layout/AppLayout'
