a99f92d7f2bb4a33e96ea5275b0fb53b16ecff13
priscillanabachwa <priscillanabachwa4@gmail.com>
2026-05-05 12:05:24 +0300

    updated the placements models and view.py

--- backend/placements/views.py
+++ backend/placements/views.py
@@ -14,7 +14,16 @@ class PlacementViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     filter_backends = [filters.SearchFilter]
-    search_fields = ['student__username', 'company_name']
+    search_fields = ['student__username', 'company__company_name']
 
-
+    def get_queryset(self):
+        user = self.request.user
+        if user.role == 'student':
+            return InternshipPlacement.objects.filter(student=user)
+        elif user.role == 'workplace_supervisor':
+            return InternshipPlacement.objects.filter(workplace_supervisor=user)
+        elif user.role == 'academic_supervisor':
+            return InternshipPlacement.objects.filter(academic_supervisor=user)
+        return super().get_queryset()
+          
 
