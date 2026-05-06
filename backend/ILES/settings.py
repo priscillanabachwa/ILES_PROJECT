d9d6f1cfea824f229842c024df622155bb0c3e53
naluyimalaureen-source <naluyimalaureen@gmail.com>
2026-05-06 22:54:06 +0300

    added the twilio credentials in the settings.py.

--- backend/ILES/settings.py
+++ backend/ILES/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -194,3 +195,8 @@ CACHES = {
         'LOCATION': 'iles-cache',
     }
 }
+
+# ==================== Twilio SMS Configuration ====================
+TWILIO_ACCOUNT_SID = config('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = config('TWILIO_AUTH_TOKEN')
+TWILIO_PHONE_NUMBER = config('TWILIO_PHONE_NUMBER')
