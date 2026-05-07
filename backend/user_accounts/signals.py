c33d97793ea88de03fb06a9b06a02615fad39d0a
priscillanabachwa <priscillanabachwa4@gmail.com>
2026-05-07 10:32:57 +0300

    implemeted permissions systems for placements

--- backend/user_accounts/signals.py
+++ backend/user_accounts/signals.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import Group
 from .models import CustomUser
 from django.core.mail import send_mail
 from django.conf import settings
@@ -19,3 +20,14 @@ def send_welcome_email(sender, instance, created, **kwargs):
             recipient_list= recipient_list,
             fail_silently=False,
         )
+
+@receiver(post_save, sender=CustomUser)
+def assign_user_to_group(sender, instance, created, **kwargs):
+    if created:
+        if hasattr(instance, 'role'): 
+            role_name = instance.role.capitalize() + 's'  # Convert role to group name (e.g., 'Student' -> 'Students')
+            try:
+                group = Group.objects.get(name=role_name)
+                instance.groups.add(group)
+            except Group.DoesNotExist:
+                pass
