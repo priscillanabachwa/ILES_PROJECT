571f5c21da91b9d065252d88a930d78e259bec8b
priscillanabachwa <priscillanabachwa4@gmail.com>
2026-05-08 03:52:34 +0300

    made migrations

--- backend/weekly_logs/signals.py
+++ backend/weekly_logs/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_migrate
 from django.dispatch import receiver
 from .models import WeeklyLogbook
 from django.core.mail import send_mail
