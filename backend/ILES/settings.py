7ca1ceacc257abcbb488d1ff606304b2d3277d46
atwiinesheila02-web <atwiinesheila02@gmail.com>
2026-06-11 16:01:12 +0100

    Changes

--- backend/ILES/settings.py
+++ backend/ILES/settings.py
@@ -88,10 +88,12 @@ WSGI_APPLICATION = 'ILES.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600
+        default=os.environ.get("DATABASE_URL") or f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
     )
 }
+
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
